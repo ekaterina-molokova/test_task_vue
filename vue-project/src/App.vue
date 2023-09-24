8b095bc2a483dcf57a25d9bfbda2a58c6986c16d
Ekaterina Molokova <yaroslav4ik@yandex.ru>
2023-09-24 17:27:02 +0200

    List rendering

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -26,28 +26,6 @@ export default {
 ],
     }
   },
-//   const templateElement = document.querySelector(".template"); 
-// const elementsContainer = document.querySelector(".cities");
-// const templateCity = document.querySelector(".template_city"); 
-// const cityInfoContainer = document.querySelector(".cards_holder");
-// const search = document.querySelector('.search_field');
-// const searchButton = document.querySelector('.search_search-button');
-// const clearButton = document.querySelector('.search_clear-button');
-// const form = document.querySelector('.search_container');
-
-// let init = citiesList;
-
-// function render() { 
-//     console.log(init);
-//     const cities = init
-//         .map(getItem)
-//         elementsContainer.append(...cities);
-// }
-
-// function getItem(item) {
-//     const newCity = templateElement.content.cloneNode(true);
-//     const elementTitle = newCity.querySelector(".city_name"); 
-//     elementTitle.textContent = item.city_name;
 
 //     elementTitle.addEventListener("click", function(evt) {
 //         elementTitle.classList.toggle('clicked');
@@ -58,21 +36,7 @@ export default {
 //         ? cityInfoContainer.prepend(...filteredCity)
 //         : (cityInfoContainer.querySelector(".card").remove() && elementTitle.classList.toggle('clicked'));
 //     });
-//     return newCity;
-// }
-
-// function getCurrentCity(item) {
-//     const currentCity = templateCity.content.cloneNode(true);
-//     const cityName = currentCity.querySelector(".city_name_card");
-//     const countryName = currentCity.querySelector(".country_name");
-//     const population = currentCity.querySelector(".population");
-//     const coordinates = currentCity.querySelector(".coordinates");
-//     cityName.textContent = item.city_name;
-//     countryName.textContent = `Country: ${item.country_name}`;
-//     population.textContent = `Population: ${item.population}`;
-//     coordinates.textContent = `Coordinates: lat ${item.coordinates.lat}, lng ${item.coordinates.lng}`;
-//     return currentCity;
-// }
+//     
 
 // search.addEventListener('change', searchFunc);
 // clearButton.addEventListener('click', function(evt) {
@@ -93,8 +57,6 @@ export default {
 //     render();
 //     return init;
 // }
-
-// render();
 }
 </script>
 
@@ -114,23 +76,29 @@ export default {
                          required
                   />
                 </fieldset>
-                <button v-bind:class="!isVisible && 'invisible'" class='search_clear-button' type='button'>X</button>
+                <button class='search_clear-button' type='button'>X</button>
               </form>
-            <template class="template">
-                <div class="cities__element">
-                    <button v-bind:class="isClicked && 'clicked'" class="city_name"></button>
-                </div>
-            </template> 
+              <div class="cities__element">
+                <button
+                  v-for="city in citiesList"
+                  :key="city.city_name"
+                  v-bind:class="isClicked && 'clicked'"
+                  class="city_name">
+                  {{ city.city_name }}
+                  </button>
+              </div>
         </section>
         <section class="cards_holder">
-            <template class="template_city">
-                <div class="card">
-                    <h2 class="city_name_card"></h2>
-                    <h3 class="country_name"></h3>
-                    <h4 class="population"></h4>
-                    <h5 class="coordinates"></h5>
-                </div>
-            </template>
+            <div
+              v-for="city in citiesList"
+              :key="city.city_name"
+              v-show="isVisible"
+              class="card">
+                <h2 class="city_name_card">{{ city.city_name }}</h2>
+                <h3 class="country_name">{{ city.country_name }}</h3>
+                <h4 class="population"> {{ city.population }}</h4>
+                <h5 class="coordinates">{{ city.coordinates }}</h5>
+            </div>
         </section>
     </main>
   </div>
@@ -139,160 +107,162 @@ export default {
 <style scoped>
 
 .header {
-    display: flex;
-    width: 30%;
-    margin: 0;
-    padding: 30px 0;
-    box-sizing: border-box;
-    color: #e96acf;
-    align-items: center;
+  border: 10px solid pink;
+  display: flex;
+  width: 100%;
+  margin: 0;
+  padding: 30px 0;
+  box-sizing: border-box;
+  color: #e96acf;
+  align-items: center;
 }
 
 .main_title {
-    margin: 0;
-    padding: 0 50px;
-    text-transform: uppercase;
-    font-size: 35px;
-    font-weight: 500;
+  margin: 0;
+  padding: 0 50px;
+  text-transform: uppercase;
+  font-size: 35px;
+  font-weight: 500;
 }
 
 .main_subtitle {
-    font-size: 15px;
-    font-weight: 300;
+  font-size: 15px;
+  font-weight: 300;
 }
 
 .main {
-    margin: 0 auto;
-    padding: 0;
-    box-sizing: border-box;
-    display: flex;
+  border: 10px solid blue;
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
 }
 
 .cities {
-    display: flex;
-    flex-direction: column;
-    border-right: 3px solid #15dfa3;
-    min-width: 30%;
-    margin: auto;
-    padding: 0;
-    box-sizing: border-box;
+  border: 10px solid green;
+  display: flex;
+  flex-direction: column;
+  min-width: 30%;
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 .cities__element {
-    margin: 10px auto;
-    padding: 0;
-    box-sizing: border-box;
+  margin: 10px auto;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 .city_name, .city_name_card, .country_name, .population, .coordinates  {
-    text-align: center;
-    margin: 0 auto;
-    padding: 0 0 5px;
-    font-weight: 500;
-    color: #e96acf;
-    border: none;
-    width: 100%;
-    background-color: transparent;
+  text-align: center;
+  margin: 0 auto;
+  padding: 5px 0;
+  font-weight: 500;
+  color: #e96acf;
+  border: none;
+  width: 100%;
+  background-color: transparent;
 }
 
 .city_name {
-    font-size: 20px;
-    text-transform: uppercase;
-    color: #15dfa3;
+  font-size: 20px;
+  text-transform: uppercase;
+  color: #15dfa3;
 }
 
 .city_name:hover {
-    cursor: pointer;
-    color: #e96acf;
+  cursor: pointer;
+  color: #e96acf;
 }
 
 .clicked {
-    border: 2px solid #e96acf;
-    padding: 10px;
-    color: #e96acf;
+  border: 2px solid #e96acf;
+  padding: 10px;
+  color: #e96acf;
 }
 
 .cards_holder {
-    width: 100%;
+  border: 10px solid yellow;
+  width: 100%;
 }
 
 .card {
-    margin: 0;
-    padding: 20px 0;
-    box-sizing: border-box;
-    border-bottom: 2px solid #15dfa3;
+  margin: 0;
+  padding: 20px 0;
+  box-sizing: border-box;
 }
 
 .search_container {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 10px 10px;
-    padding: 0;
-    box-sizing: border-box;
-    border: 2px solid #15dfa3;
-    border-radius: 25px;
-    background-color: #FFE6FA;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 0 10px;
+  padding: 0;
+  box-sizing: border-box;
+  border: 2px solid #15dfa3;
+  border-radius: 25px;
+  background-color: #FFE6FA;
+  align-items: center;
 }
 
 .search_fieldset {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    width: 100%;
-    border: none;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 100%;
+  border: none;
 }
 
 .search_field {
-    margin: 0;
-    padding: 10px;
-    box-sizing:border-box;
-    border-radius: 25px;
-    border: none;
-    width: 100%;
-    background-color: transparent;
-    font-weight: 500;
-    font-size: 15px;
-    color: #e96acf;
+  margin: 0;
+  padding: 10px;
+  box-sizing:border-box;
+  border-radius: 25px;
+  border: none;
+  width: 100%;
+  background-color: transparent;
+  font-weight: 500;
+  font-size: 15px;
+  color: #e96acf;
 }
 
 .search_field::placeholder {
-    font-weight: 500;
-    font-size: 15px;
-    color: #e96acf;
-    opacity: 0.5;
+  font-weight: 500;
+  font-size: 15px;
+  color: #e96acf;
+  opacity: 0.5;
 }
 
 .search_field:focus {
-    outline: #e96acf;
+  outline: #e96acf;
 }
 
 .search_clear-button {
-    border: none;
-    background-color: transparent;
-    width: 45px;
-    height: 45px;
-    font-size: 20px;
-    color: #e96acf;
+  border: none;
+  background-color: transparent;
+  width: 45px;
+  height: 45px;
+  font-size: 20px;
+  color: #e96acf;
 }
 
 .search_clear-button:hover {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .invisible {
-    display: none;
+  display: none;
 }
 
 @media screen and (min-width: 1200px) {
-    .main {
-        min-width: 1200px;
-    }
+  .main {
+    min-width: 1200px;
+  }
 }
 
 @media screen and (max-width: 425px) {
-    .cities {
-        display: none;
-    }
+  .cities {
+    display: none;
+  }
 }
 </style>
