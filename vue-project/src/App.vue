31ab0f52d586da460cf098cdbeb9f894fb1521be
Ekaterina Molokova <yaroslav4ik@yandex.ru>
2023-10-06 18:37:05 +0200

    finishing computed functionality

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -10,12 +10,27 @@ export default {
   methods: {
     handleClick(event) {
       this.isClicked = !this.isClicked;
-      this.isClicked
-      ? this.currentCity = computed(() => {
-        return this.citiesList.filter(item => item.city_name === event.target.innerHTML)})
-      : this.currentCity = computed(() => {
-        return {}
-      });
+      // this.isClicked
+      // ? this.currentCity = computed(() => {
+      //   return this.citiesList.filter(item => {
+      //     if (item.city_name === event.target.innerHTML) {
+      //       item.clicked = true;
+      //       return item;
+      //     }
+      //   })})
+      // : this.currentCity = computed(() => {
+      //   return {}
+      // });
+      this.currentCity = computed(() => {
+        return this.citiesList.filter(item => {
+          if (item.city_name === event.target.innerHTML && item.clicked === false) {
+            item.clicked = true;
+            return item;
+          } else {
+            item.clicked = false;
+          }
+        })});
+
     },
     handleSearch(event) {
       event.preventDefault();
@@ -53,21 +68,21 @@ export default {
         search: '',
       },
       citiesList: [
-	{ "city_name": "Amsterdam", "country_name": "Netherlands", "population": 872757, "coordinates": { "lat": 52.3676, "lng": 4.9041 } },
-	{ "city_name": "Rotterdam", "country_name": "Netherlands", "population": 650000, "coordinates": { "lat": 51.9225, "lng": 4.47917 } },
-	{ "city_name": "The Hague", "country_name": "Netherlands", "population": 540000, "coordinates": { "lat": 52.0787, "lng": 4.2888 } },
-	{ "city_name": "Paris", "country_name": "France", "population": 2140526, "coordinates": { "lat": 48.8566, "lng": 2.3522 } },
-	{ "city_name": "Marseille", "country_name": "France", "population": 861635, "coordinates": { "lat": 43.2965, "lng": 5.3698 } },
-	{ "city_name": "Lyon", "country_name": "France", "population": 515695, "coordinates": { "lat": 45.7597, "lng": 4.8422 } },
-	{ "city_name": "Berlin", "country_name": "Germany", "population": 3769495, "coordinates": { "lat": 52.52, "lng": 13.405 } },
-	{ "city_name": "Hamburg", "country_name": "Germany", "population": 1822445, "coordinates": { "lat": 53.5511, "lng": 9.9937 } },
-	{ "city_name": "Munich", "country_name": "Germany", "population": 1471508, "coordinates": { "lat": 48.1351, "lng": 11.582 } },
-	{ "city_name": "New York", "country_name": "USA", "population": 8175133, "coordinates": { "lat": 40.7128, "lng": -74.006 } },
-	{ "city_name": "Los Angeles", "country_name": "USA", "population": 3792621, "coordinates": { "lat": 34.0522, "lng": -118.2437 } },
-	{ "city_name": "Chicago", "country_name": "USA", "population": 2695598, "coordinates": { "lat": 41.8781, "lng": -87.6298 } },
-	{ "city_name": "Moscow", "country_name": "Russia", "population": 12506468, "coordinates": { "lat": 55.7558, "lng": 37.6176 } },
-	{ "city_name": "Saint Petersburg", "country_name": "Russia", "population": 5351935, "coordinates": { "lat": 59.9343, "lng": 30.3351 } },
-	{ "city_name": "Novosibirsk", "country_name": "Russia", "population": 1612833, "coordinates": { "lat": 55.0084, "lng": 82.9357 } }
+	{ "city_name": "Amsterdam", "country_name": "Netherlands", "population": 872757, "coordinates": { "lat": 52.3676, "lng": 4.9041, "clicked": false } },
+	{ "city_name": "Rotterdam", "country_name": "Netherlands", "population": 650000, "coordinates": { "lat": 51.9225, "lng": 4.47917, "clicked": false } },
+	{ "city_name": "The Hague", "country_name": "Netherlands", "population": 540000, "coordinates": { "lat": 52.0787, "lng": 4.2888, "clicked": false } },
+	{ "city_name": "Paris", "country_name": "France", "population": 2140526, "coordinates": { "lat": 48.8566, "lng": 2.3522, "clicked": false } },
+	{ "city_name": "Marseille", "country_name": "France", "population": 861635, "coordinates": { "lat": 43.2965, "lng": 5.3698, "clicked": false } },
+	{ "city_name": "Lyon", "country_name": "France", "population": 515695, "coordinates": { "lat": 45.7597, "lng": 4.8422, "clicked": false } },
+	{ "city_name": "Berlin", "country_name": "Germany", "population": 3769495, "coordinates": { "lat": 52.52, "lng": 13.405, "clicked": false } },
+	{ "city_name": "Hamburg", "country_name": "Germany", "population": 1822445, "coordinates": { "lat": 53.5511, "lng": 9.993, "clicked": false } },
+	{ "city_name": "Munich", "country_name": "Germany", "population": 1471508, "coordinates": { "lat": 48.1351, "lng": 11.582, "clicked": false } },
+	{ "city_name": "New York", "country_name": "USA", "population": 8175133, "coordinates": { "lat": 40.7128, "lng": -74.006, "clicked": false } },
+	{ "city_name": "Los Angeles", "country_name": "USA", "population": 3792621, "coordinates": { "lat": 34.0522, "lng": -118.2437, "clicked": false } },
+	{ "city_name": "Chicago", "country_name": "USA", "population": 2695598, "coordinates": { "lat": 41.8781, "lng": -87.6298, "clicked": false } },
+	{ "city_name": "Moscow", "country_name": "Russia", "population": 12506468, "coordinates": { "lat": 55.7558, "lng": 37.6176, "clicked": false } },
+	{ "city_name": "Saint Petersburg", "country_name": "Russia", "population": 5351935, "coordinates": { "lat": 59.9343, "lng": 30.3351, "clicked": false } },
+	{ "city_name": "Novosibirsk", "country_name": "Russia", "population": 1612833, "coordinates": { "lat": 55.0084, "lng": 82.9357, "clicked": false } }
 ],
     }
   },
@@ -95,14 +110,15 @@ export default {
                 <button class='search_clear-button' v-bind:class="isVisible ? '' : 'invisible'" type='button' @click="handleClear">X</button>
             </form>
             <div class="cities__element"
-              v-for="city in citiesList"
-              :key="city.city_name"
+              v-for="({ city_name, clicked }) in citiesList"
+              :key="city_name"
               @click="handleClick"
             >
             <button
               class="city_name"
+              :class="clicked ? 'clicked' : ''"
             >
-              {{ city.city_name }}
+              {{ city_name }}
             </button>
             </div>
         </section>
